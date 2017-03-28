5304c03f5b8fee7a6654c5ba4e12704b6555e829
Kimberley Hood <KimberleyHood@Kimberleys-MacBook-Pro.local>
2017-03-28 06:57:39 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,55 @@
 
 
+     //listing out my variables
      var words = ["khaleesi", "jon snow", "sansa", "the mountain", "tyrion", "drogo"];
      var wins = 0;
      var guessesLeft = 15;
      var answerString = []
-     var guesses = [];
-    
-     var pickWord = words[Math.floor(Math.random() * words.length)]; 
-          
+     var guesses = "";
+     var wrongGuesses = [];
+     var chosenWord = "";
      
-    // Display empty placeholders for answer    
+          
+     function startGame() {
+  
 
-    for (i = 0; i < pickWord.length; i++) {
-        answerString[i] = "_ ";
+     var wins = 0;
+     var guessesLeft = 15;
+     var wrongGuesses = [];
+     var pickWord = words[Math.floor(Math.random() * words.length)];
+
+     answerString = pickWord.split("");
+    
+
+    /*for (i = 0; i < pickWord.length; i++) {
+        answerString[i] = "_ "; console.log(answerString)
     }
 
-        var el = document.getElementById("placeholders");
+       var el = document.getElementById("placeholders");
         el.innerHTML = answerString;
-        el.innerHTML = answerString.join("_ ");
-
+        el.innerHTML = answerString.join("_ ");*/
 
     
+    document.getElementById("guessesleft").innerHTML = wrongGuesses.join(" ");
 
+    
+}
+
+function checkLetters() {
+
+}
+
+function reset() {
+
+}
+    
+    document.keyup = function(event) {
+     
+
+        
+    
+        
+};
     
 
     
